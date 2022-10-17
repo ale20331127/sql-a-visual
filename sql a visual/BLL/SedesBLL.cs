49669f2cb92cec31a1b5ca651e1bce295539cd8a
alexa <alexa@DESKTOP-NAVB3UH>
2022-10-16 21:45:31 -0600

    Agregar archivos de proyecto.

---
+++ sql a visual/BLL/SedesBLL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sql_a_visual.BLL
+{
+    public class SedesBLL
+    {
+        private int id;
+        private string nombre;
+        private string ubicacion;
+
+        public SedesBLL(int id)
+        {
+            this.id = id;
+        }
+
+        public SedesBLL(int id, string nombre, string ubicacion)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.ubicacion = ubicacion;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Ubicacion { get => ubicacion; set => ubicacion = value; }
+    }
+}
---
+++ sql a visual/DAL/Database.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using sql_a_visual.Properties;
+using System.Configuration;
+
+namespace sql_a_visual.DAL
+{
+    public class Database
+    {
+        public static string getStrConnection()
+        {
+            return Settings.Default.empleadosConnectionString;
+        }
+
+        public SqlConnection getConnection()
+        {
+            SqlConnection Con = new SqlConnection(getStrConnection());
+            return Con;
+        }
+
+    }
+}
